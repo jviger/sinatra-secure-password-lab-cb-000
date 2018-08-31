bbaa486c1e44a337146f3b7fdba50a4e54531e97
Jason Viger <jason.viger@gmail.com>
2018-08-31 10:35:46 +0000

    automatically backed up by learn

--- db/migrate/20180831103359_create_user.rb
+++ db/migrate/20180831103359_create_user.rb
@@ -1,6 +1,6 @@
 class CreateUser < ActiveRecord::Migration
   def change
-    create_table :products do |t|
+    create_table :users do |t|
       t.string :username
       t.string :password
     end
